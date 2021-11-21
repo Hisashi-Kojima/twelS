54043c84101a5f77c4e04f310a25694f7df3c789
Hisashi <kakujo.hk@gmail.com>
2021-11-22 00:45:17 +0900

    parse mtext

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -867,6 +867,98 @@ def test_get_parsed_tree_table_3():
     assert expected == Parser.get_parsed_tree(mathml)
 
 
+def test_get_parsed_tree_mspace_1():
+    """
+    mspaceを含む式
+    参考ページ：跡（線形代数学）
+    https://ja.wikipedia.org/wiki/%E8%B7%A1_(%E7%B7%9A%E5%9E%8B%E4%BB%A3%E6%95%B0%E5%AD%A6)
+    """
+    mathml = """<math xmlns="http://www.w3.org/1998/Math/MathML"  alttext="{\displaystyle B(x,y)=\operatorname {tr} (\operatorname {ad} (x)\operatorname {ad} (y))\qquad (\operatorname {ad} (x)y:=[x,y]=xy-yx)}">
+                    <semantics>
+                        <mrow class="MJX-TeXAtom-ORD">
+                        <mstyle displaystyle="true" scriptlevel="0">
+                            <mi>B</mi>
+                            <mo stretchy="false">(</mo>
+                            <mi>x</mi>
+                            <mo>,</mo>
+                            <mi>y</mi>
+                            <mo stretchy="false">)</mo>
+                            <mo>=</mo>
+                            <mi>tr</mi>
+                            <mo>&#x2061;<!-- ⁡ --></mo>
+                            <mo stretchy="false">(</mo>
+                            <mi>ad</mi>
+                            <mo>&#x2061;<!-- ⁡ --></mo>
+                            <mo stretchy="false">(</mo>
+                            <mi>x</mi>
+                            <mo stretchy="false">)</mo>
+                            <mi>ad</mi>
+                            <mo>&#x2061;<!-- ⁡ --></mo>
+                            <mo stretchy="false">(</mo>
+                            <mi>y</mi>
+                            <mo stretchy="false">)</mo>
+                            <mo stretchy="false">)</mo>
+                            <mspace width="2em" />
+                            <mo stretchy="false">(</mo>
+                            <mi>ad</mi>
+                            <mo>&#x2061;<!-- ⁡ --></mo>
+                            <mo stretchy="false">(</mo>
+                            <mi>x</mi>
+                            <mo stretchy="false">)</mo>
+                            <mi>y</mi>
+                            <mo>:=</mo>
+                            <mo stretchy="false">[</mo>
+                            <mi>x</mi>
+                            <mo>,</mo>
+                            <mi>y</mi>
+                            <mo stretchy="false">]</mo>
+                            <mo>=</mo>
+                            <mi>x</mi>
+                            <mi>y</mi>
+                            <mo>&#x2212;<!-- − --></mo>
+                            <mi>y</mi>
+                            <mi>x</mi>
+                            <mo stretchy="false">)</mo>
+                        </mstyle>
+                        </mrow>
+                        <annotation encoding="application/x-tex">{\displaystyle B(x,y)=\operatorname {tr} (\operatorname {ad} (x)\operatorname {ad} (y))\qquad (\operatorname {ad} (x)y:=[x,y]=xy-yx)}</annotation>
+                    </semantics>
+                </math>"""
+    # TODO: expectedの実装
+    expected = False
+    assert expected == Parser.get_parsed_tree(mathml)
+
+
+def test_get_parsed_tree_mtext_1():
+    """
+    mtextを含む式
+    """
+    mathml = """<math xmlns="http://www.w3.org/1998/Math/MathML"  alttext="{\displaystyle {\text{inv}}(A)=\#\{(A_{i},A_{j})\mid i&lt;j{\text{ and }}A_{i}&gt;A_{j}\}}">
+                    <semantics>
+                        <mrow class="MJX-TeXAtom-ORD">
+                            <mstyle displaystyle="true" scriptlevel="0">
+                                <mrow class="MJX-TeXAtom-ORD">
+                                    <mtext>inv</mtext>
+                                </mrow>
+                                <mo stretchy="false">(</mo>
+                                <mi>A</mi>
+                                <mo stretchy="false">)</mo>
+                            </mstyle>
+                        </mrow>
+                        <annotation encoding="application/x-tex">{\displaystyle {\text{inv}}(A)=\#\{(A_{i},A_{j})\mid i&lt;j{\text{ and }}A_{i}&gt;A_{j}\}}</annotation>
+                    </semantics>
+                </math>"""
+    expected = Tree(Const.root_data, [
+            Tree(Const.product_data, [
+                Token(Const.token_type, 'inv'),
+                Tree(Const.paren_data, [
+                    Token(Const.token_type, 'A')
+                ])
+            ])
+        ])
+    assert expected == Parser.get_parsed_tree(mathml)
+
+
 def test_get_parsed_tree_cdots_1():
     """0.999...のparse
     https://ja.wikipedia.org/wiki/%E7%B4%9A%E6%95%B0
