3bb82f72b120ab2c5bcaaee7a7a9af31bb536827
Hisashi <kakujo.hk@gmail.com>
2021-12-28 22:13:00 +0900

    create a test file

---
+++ tests/test_spider.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""module description
+made by Hisashi
+"""
+
+from wiki_crawler.wiki_crawler.spiders.local_math_spider import LocalMathSpider
+
+
+# TODO: implement this test.
+def test_foo():
+    assert True is False
