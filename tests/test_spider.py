c3f24c29340b36622c13e3a41ccf07e061735c56
Hisashi <kakujo.hk@gmail.com>
2022-01-26 17:35:32 +0900

    fix ModuleNotFoundError

--- tests/test_spider.py
+++ tests/test_spider.py
@@ -3,7 +3,24 @@
 made by Hisashi
 """
 
+import os
+import subprocess
+
 from wiki_crawler.wiki_crawler.spiders.local_math_spider import LocalMathSpider
+
+
+def test_crawl_1():
+    """正常にクロールできるか確認．
+    このテストがsubprocess.CalledProcessErrorで失敗したとき，
+    terminalで'scrapy list'を実行すると，エラーになるだろう．
+    クロール時にModuleNotFoundError: No module named 'crawler.settings'が
+    出るときにはこのテストは失敗する．
+    """
+    os.chdir('../wiki_crawler')  # move to scrapy root directory
+
+    command = ['scrapy', 'list']
+    subprocess.run(command, check=True)
+    assert True
 
 
 def test_get_domain_from_uri_1():
