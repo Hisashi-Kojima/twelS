26f2fe88598d9934899c5c568f3a34506875e103
Hisashi <kakujo.hk@gmail.com>
2022-12-08 17:05:45 +0900

    change the url

--- front/search/urls.py
+++ front/search/urls.py
@@ -9,8 +9,7 @@ from front.search.views import index, robots_txt, coming_soon, privacy_policy
 app_name='search'
 
 urlpatterns = [
-    path('', coming_soon),
-    path('314159265358979/', index, name='index'),
+    path('', index, name='index'),
     path('privacypolicy/', privacy_policy, name='privacy_policy'),
     path('robots.txt', robots_txt),
 ]
