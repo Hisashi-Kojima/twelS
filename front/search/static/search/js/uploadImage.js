fbb4da2b9db50e58b6efc66d663f050fd4aa2831
KusumotoTakahiro <gantaka921@icloud.com>
2023-03-09 23:27:03 +0900

    Change index.html to load uploadImage.js

--- front/search/static/search/js/uploadImage.js
+++ front/search/static/search/js/uploadImage.js
@@ -1,14 +1,14 @@
 function uploadImage() {
-  const uploadImageBtn = document.getElementById('uploadImage');
-  // for camera: setting a event which a file input was changed.
-  uploadImageBtn.addEventListener(
-      "change",
-      (e) => {// when the file uploaded, the form is posted py this code.
-          const imageForm = document.getElementById('imageForm');
-          imageForm.submit();
-      }
-  )
-  uploadImageBtn.click();
+    const uploadImageBtn = document.getElementById('uploadImage');
+    // for camera: setting a event which a file input was changed.
+    uploadImageBtn.addEventListener(
+        "change",
+        (e) => {// when the file uploaded, the form is posted py this code.
+            const imageForm = document.getElementById('imageForm');
+            imageForm.submit();
+        }
+    )
+    uploadImageBtn.click();
 }
 
 module.exports = uploadImage
