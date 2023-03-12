07170bee506e7c5b249dc9045e0d669ecf764b6a
dorapon48 <dorapon48@icloud.com>
2023-03-12 15:16:38 +0900

    add selenium login test

--- tests/test_email_login.py
+++ tests/test_email_login.py
@@ -6,6 +6,10 @@ from django.shortcuts import redirect
 from django.test.utils import override_settings
 from create_webdriver import Create_Driver_ConnectedHub
 from django.test import LiveServerTestCase
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from django.conf import settings
 
 
 
@@ -22,7 +26,7 @@ class EmailLoginTest(TestCase):
     def test_csrf(self):
         """csrfトークンを含むこと"""
         self.assertContains(self.response, 'csrfmiddlewaretoken')
-
+'''
 class SuccessfulEmailLoginTests(LiveServerTestCase):
     host = 'python'
     """ユーザー登録成功時のテスト"""
@@ -87,6 +91,94 @@ class SuccessfulEmailLoginTests(LiveServerTestCase):
             driver.get(url)
             title = driver.title
             assert title == 'メール認証が完了しました'
+        
+        self.response = self.client.get(reverse('search:index'))
+        #self.assertEqual(self.response.status_code, 200) # なぜか302になる為要検証
+        self.assertEqual(self.response.status_code, 302)
+        self.assertTrue(EmailUser.objects.get(email="test@edu.cc.saga-u.ac.jp").is_active)
+'''
+class SeleniumEmailLoginTests(LiveServerTestCase):
+    host = 'python'
+    """seleniumによるログインテスト"""
+    def setUp(self):
+        emailuser = EmailUser.objects.filter(email="test@edu.cc.saga-u.ac.jp")
+        self.assertQuerysetEqual(emailuser, [])
+        # テスト中はCSRFの検証を切っておく
+        settings.CSRF_COOKIE_SECURE = False
+        settings.SESSION_COOKIE_SECURE = False
+        #self.selenium = Create_UnConected_Driver("chrome", 110)
+    # ----tests----
+    # ブラウザごとにテストする
+    # 最新バージョン->
+    # chrome v110
+    def test_latest_chrome(self):
+        self.email_login("chrome", 110)
+    '''
+    # firefox v110
+    def test_latest_firefox(self):
+        self.email_login("firefox", 110)
+    # edge v110
+    def test_latest_edge(self):
+        self.email_login("edge", 110)
+    # 確認できた最も古いバージョン->
+    # chrome v61
+    def test_oldest_chrome(self):
+        self.email_login("chrome", 61)
+    # firefox v88
+    def test_oldest_firefox(self):
+        self.email_login("firefox", 88)
+    # edge v92
+    def test_oldest_edge(self):
+        self.email_login("edge", 92)
+    '''
+    # -------------
+    @override_settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend')  # メールのテストのために上書き
+    def email_login(self, browser, version):
+
+        url = self.live_server_url
+        self.response = self.client.get(reverse('login:email_login'))
+        # seleniumでアクセス
+        with Create_Driver_ConnectedHub(browser, version) as driver:
+            wait = WebDriverWait(driver=driver, timeout=30)
+            # リダイレクトでログインページに飛ぶ
+            driver.get(url)
+            # 要素が表示されるまで待つ
+            wait.until(EC.presence_of_all_elements_located)
+            assert driver.title == 'ログイン'
+            # メールアドレスでログインページに移行
+            element = driver.find_element(By.LINK_TEXT, "メールアドレスでのログイン")
+            element.click()
+            wait.until(EC.presence_of_all_elements_located)
+            assert driver.title == 'メールでのログイン'
+            # inputにメールアドレスを入力してボタンをクリック
+            element = driver.find_element(By.NAME, "email")
+            element.send_keys("test@edu.cc.saga-u.ac.jp")
+            button = driver.find_elements(By.TAG_NAME, "button")
+            for b in button:
+                if b.text == 'ログイン':
+                    b.click()
+                    break
+            wait.until(EC.presence_of_all_elements_located)
+            assert driver.title == 'メールを送信しました'
+
+        self.assertEqual(len(mail.outbox), 1)  # 1通のメールが送信されていること
+        self.assertEqual(mail.outbox[0].from_email, '22801001@edu.cc.saga-u.ac.jp')  # 送信元
+        self.assertEqual(mail.outbox[0].to, ['test@edu.cc.saga-u.ac.jp'])  # 宛先
+
+        body_lines = mail.outbox[0].body.split('\n')
+        url = body_lines[5]  # メール本文から認証urlを取得
+
+        self.assertIn('http://127.0.0.1:8000/login/email_login/complete/', url)
+
+        # 一度でもアクセスすると2回目以降400エラーを返されるのでコメントアウト
+        #self.response = self.client.get(url)
+        #self.assertEqual(self.response.status_code, 200)
+
+        # テスト用のURLに変更する
+        url = url.replace("http://127.0.0.1:8000", self.live_server_url)
+
+        # seleniumでアクセスし確認
+        
         
         self.response = self.client.get(reverse('search:index'))
         #self.assertEqual(self.response.status_code, 200) # なぜか302になる為要検証
