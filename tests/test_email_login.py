c968f95958abf79d580a7c06b28fda1ee003ce3d
dorapon48 <dorapon48@icloud.com>
2023-03-12 16:24:07 +0900

    update selenium login test

--- tests/test_email_login.py
+++ tests/test_email_login.py
@@ -136,7 +136,8 @@ class SeleniumEmailLoginTests(LiveServerTestCase):
     def email_login(self, browser, version):
 
         url = self.live_server_url
-        self.response = self.client.get(reverse('login:email_login'))
+        # 送信先
+        e_address = "test@edu.cc.saga-u.ac.jp"
         # seleniumでアクセス
         with Create_Driver_ConnectedHub(browser, version) as driver:
             wait = WebDriverWait(driver=driver, timeout=30)
@@ -152,7 +153,7 @@ class SeleniumEmailLoginTests(LiveServerTestCase):
             assert driver.title == 'メールでのログイン'
             # inputにメールアドレスを入力してボタンをクリック
             element = driver.find_element(By.NAME, "email")
-            element.send_keys("test@edu.cc.saga-u.ac.jp")
+            element.send_keys(e_address)
             button = driver.find_elements(By.TAG_NAME, "button")
             for b in button:
                 if b.text == 'ログイン':
@@ -160,27 +161,22 @@ class SeleniumEmailLoginTests(LiveServerTestCase):
                     break
             wait.until(EC.presence_of_all_elements_located)
             assert driver.title == 'メールを送信しました'
+            # メールの確認
+            self.assertEqual(len(mail.outbox), 1)  # 1通のメールが送信されていること
+            self.assertEqual(mail.outbox[0].from_email, '22801001@edu.cc.saga-u.ac.jp')  # 送信元
+            self.assertEqual(mail.outbox[0].to, [e_address])  # 宛先
+            # 認証URLを取得
+            body_lines = mail.outbox[0].body.split('\n')
+            url = body_lines[5]  # メール本文から認証urlを取得
+            self.assertIn(self.live_server_url + '/login/email_login/complete/', url)
+            # 認証URLにアクセス
+            driver.get(url)
+            wait.until(EC.presence_of_all_elements_located)
+            assert driver.title == 'メール認証が完了しました'
+            element = driver.find_element(By.LINK_TEXT, "ログイン完了")
+            element.click()
+            wait.until(EC.presence_of_all_elements_located)
+            assert driver.title == 'twelS'
+            self.assertTrue(EmailUser.objects.get(email=e_address).is_active)
 
-        self.assertEqual(len(mail.outbox), 1)  # 1通のメールが送信されていること
-        self.assertEqual(mail.outbox[0].from_email, '22801001@edu.cc.saga-u.ac.jp')  # 送信元
-        self.assertEqual(mail.outbox[0].to, ['test@edu.cc.saga-u.ac.jp'])  # 宛先
-
-        body_lines = mail.outbox[0].body.split('\n')
-        url = body_lines[5]  # メール本文から認証urlを取得
-
-        self.assertIn('http://127.0.0.1:8000/login/email_login/complete/', url)
-
-        # 一度でもアクセスすると2回目以降400エラーを返されるのでコメントアウト
-        #self.response = self.client.get(url)
-        #self.assertEqual(self.response.status_code, 200)
-
-        # テスト用のURLに変更する
-        url = url.replace("http://127.0.0.1:8000", self.live_server_url)
-
-        # seleniumでアクセスし確認
         
-        
-        self.response = self.client.get(reverse('search:index'))
-        #self.assertEqual(self.response.status_code, 200) # なぜか302になる為要検証
-        self.assertEqual(self.response.status_code, 302)
-        self.assertTrue(EmailUser.objects.get(email="test@edu.cc.saga-u.ac.jp").is_active)
