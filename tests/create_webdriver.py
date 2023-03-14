504c8b35ea84d77d50ba930e87bac91b611ac7b2
dorapon48 <dorapon48@icloud.com>
2023-03-14 19:33:34 +0900

    add docstrings

--- tests/create_webdriver.py
+++ tests/create_webdriver.py
@@ -3,37 +3,39 @@ seleniumでのセッションを開始する関数群
 '''
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from typing import Tuple, Union
 
-def Create_Driver(options):
-    '''
-    hubに接続したwebdriverを返す
-    options: (object) webdriver option
-    return: (object) webdriver
-    '''
+
+def Create_Driver(options: Options) -> webdriver:
+    """hubに接続したwebdriverを返す
+    Args:
+        options: (Options) webdriver option
+    Return:
+        (webdriver) webdriver
+    """
     driver = webdriver.Remote(
         command_executor=f'http://selenium-hub:4444/wd/hub',
         options=options,
     )
     return driver
 
-def Get_BrowserOption(browser):
-    '''
-    指定したブラウザのオプションを返す
-    存在しないブラウザの時はchromeのオプションになる
-    browser: (string)
-    return: (object) browser driver options
-    '''
+def Get_BrowserOption(browser: str) -> webdriver.ChromeOptions:
+    """指定したブラウザのオプションを返す
+    存在しないブラウザが入力された時はchromeのオプションになる
+    Args:
+        browser: (string) browser name
+    Return:
+        (Options) webdriver options
+    """
+    
     if browser == "firefox" :
-        # firefox
         options = webdriver.FirefoxOptions()
     elif browser == "edge" :
-        # edge
         options = webdriver.EdgeOptions()
     elif browser == "safari" :
-        # safari
         options = webdriver.SafariOptions()
     elif browser == "chrome" :
-        # chrome
         options = webdriver.ChromeOptions()
     else:
         print("[create_webdiver] error: Specified browser is not supported")
@@ -41,13 +43,15 @@ def Get_BrowserOption(browser):
     return options
 
 def Set_Browser_Version(options, version):
-    '''
-    optionsにバージョン情報を追加する
-    注意: バージョンは110のように入力すること
-    options: (object) webdriver option
-    version: (string or int) browser version
-    return: (object) webdriver option
-    '''
+    """webdriver.Optionにバージョン情報を追加する
+    Args:
+        options (webdriver.Option) webdriver option
+        version (int, string) browser version
+    Return:
+        (webdriver) webdriver
+    Note:
+        versionに入力する数値(文字列)は正の整数であること
+    """
     str_ver = version
     if type(str_ver) is int:
         str_ver = str(str_ver) + ".0"
@@ -56,17 +60,19 @@ def Set_Browser_Version(options, version):
     options.set_capability("browserVersion", str_ver)
     return options
 
-def Create_Driver_ConnectedHub(browser_name, version):
-    '''
-    指定したブラウザでhubにのみ接続した状態のドライバーを返す\n
-    <注意> versionは正の整数を入力すること\n
-    例: Create_Conected_Driver("chrome", 110)
-    browser_name: (string) browser name
-    version: (int or string) browser version
-    return: (object) webdriver
-    '''
+def Create_Driver_ConnectedHub(browser_name: str, version: int) -> webdriver:
+    """指定したブラウザでhubにのみ接続した状態のドライバーを返す
+    Args:
+        browser_name (string) browser name
+        version (int, string) browser version
+    Return:
+        (webdriver) webdriver
+    Note:
+        versionに入力する数値(文字列)は正の整数であること
+    """
     options = Get_BrowserOption(browser_name)
     options = Set_Browser_Version(options, version)
     driver = Create_Driver(options)
     return driver
 
+print(type(Get_BrowserOption("chrome")))
