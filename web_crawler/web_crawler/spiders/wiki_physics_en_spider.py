e4946124d7dcb7663d9e509fb1c5c1e9a57b8247
RikutoKatayama <kari130212@icloud.com>
2023-03-31 18:17:51 +0900

    Add wikipedia's en.physics page .

---
+++ web_crawler/web_crawler/spiders/wiki_physics_en_spider.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""module description
+"""
+
+import scrapy
+from scrapy.http import TextResponse
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from ..items import DownloadItem
+
+
+# Read the entire file as a list
+def _load_script(path: str) -> str:
+    script = 'scriptが読み込まれていません．'
+    with open(path) as f:
+        script = f.readlines()
+    return script
+
+
+class WikiPhysicsSpider(CrawlSpider):
+    # type 'scrapy crawl wiki_physics_en' to crawl.
+    name = 'wiki_physics_en'
+    allowed_domains = [
+        'en.wikipedia.org',
+    ]
+
+    custom_settings = {
+        'ROBOTSTXT_OBEY': False,
+        'FILES_STORE': 'wiki_pages/en/physics',
+        'ITEM_PIPELINES': {'wiki_crawler.pipelines.DownloadPipeline': 300},
+    }
+
+    count = 0
+    category_urls = _load_script('wiki_crawler/spiders/category.txt')
+    start_urls = category_urls
+
+    category_path = 'wiki/Category:'
+    next_page = '/w/index'
+    rules = (
+        # extract category links to crawl all categories.
+        Rule(
+            LinkExtractor(
+                allow=(category_path, next_page),
+                restrict_xpaths=([
+                    '//a[contains(text(), "next page")]',  # for 'next page' links
+                ]),
+            )
+        ),
+        # extract page links to download.
+        Rule(
+            LinkExtractor(
+                allow=('wiki/',),
+                restrict_xpaths=([
+                    '//*[@id="mw-pages"]'
+                ]),
+            ),
+            callback='parse_item'
+        ),
+    )
+
+    def parse_item(self, response: TextResponse) -> scrapy.Item:
+        """ページを解析する関数．"""
+        return DownloadItem(
+            # replace in order to avoid FileNotFoundError because of '/'
+            title=response.css('title::text').get().replace('/', '÷'),
+            # FilesPipeline needs 'file_urls' and 'files' fields.
+            file_urls=[response.url]
+        )
