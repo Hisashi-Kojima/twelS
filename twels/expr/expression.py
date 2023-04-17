549bb074e18b4cd7e5b3a2d8848d0c8e809273c9
Hisashi <kakujo.hk@gmail.com>
2023-04-17 21:54:47 +0900

    fix the bug about mspace.

--- twels/expr/expression.py
+++ twels/expr/expression.py
@@ -50,11 +50,7 @@ class Expression:
     def _clean(mathml: str) -> str:
         """不要な情報を削除する関数。
         """
-        # BeautifulSoupが<mspace/>を<mspace></mspace>に変換するので、
-        # 変換前に<mspace/>を削除する。
-        new_mathml = __class__._remove_mspace(mathml)
-
-        soup = BeautifulSoup(new_mathml, 'lxml')
+        soup = BeautifulSoup(mathml, 'lxml')
         soup.html.unwrap()
         soup.body.unwrap()
 
@@ -64,36 +60,33 @@ class Expression:
         return tmp.replace(' ', '')
 
     @staticmethod
-    def _remove_mspace(mathml: str) -> str:
-        """MathML中の<mspace />を削除する関数。
-        Note:
-            （要確認）mspaceは数式の区切り文字として使用されることもあるようなので、
-            全てのmspaceを削除することには注意した方が良さそう。
-        """
-        return re.sub(r'<mspace.*?/>', '', mathml)
-
-    @staticmethod
     def _remove_unnecessary_tags(soup: BeautifulSoup) -> str:
         """MathMLの不要なタグを削除する関数。
         Args:
             soup: MathMLをBeautifulSoupで解析したもの。
         Notes:
-            mrowを削除すると、
-            <mfrac>
-                <mi>d</mi>
-                <mrow>
+            1.  mrowを削除すると、
+                <mfrac>
                     <mi>d</mi>
-                    <mi>x</mi>
-                </mrow>
-            </mfrac>
-            などを正しくparseできなくなってしまう。
-            なので、mrowを常に削除することはできない。
+                    <mrow>
+                        <mi>d</mi>
+                        <mi>x</mi>
+                    </mrow>
+                </mfrac>
+                などを正しくparseできなくなってしまう。
+                なので、mrowを常に削除することはできない。
+            2.  BeautifulSoupが<mspace/>を<mspace></mspace>に変換する。
+            3.  この関数ではなくExpression._clean()のBeautifulSoup呼び出し前に
+                <mspace/>を削除する方法も考えられるが、SnippetでもBeautifulSoupを呼び出しているので、
+                Expressionに渡されるMathML中に<mspace></mspace>が含まれることがある。
+            4.  （要確認）mspaceは数式の区切り文字として使用されることもあるようなので、
+                全てのmspaceを削除することには注意した方が良さそう。
         """
         save_list = [
             'semantics', 'mstyle'
         ]
         remove_list = [
-            'annotation'
+            'annotation', 'mspace'
         ]
         for item in soup.find_all(save_list):
             item.unwrap()
