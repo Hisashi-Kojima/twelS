fb289dfda1d26695d1bfaef05081b7779fbd8198
Nakagawa Yuto <n.yuuto.11.11@docomo.ne.jp>
2023-04-20 15:43:13 +0900

    fix test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
-from django.urls import reverse
 from django.core import mail
+from django.test import TestCase
 from django.test.utils import override_settings
+from django.urls import reverse
 
 User = get_user_model()
 
@@ -10,7 +10,7 @@ User = get_user_model()
 class LoginTest(TestCase):
     def setUp(self) -> None:
         self.response = self.client.get(reverse('search:index'))
-        self.assertEqual(self.response.status_code, 302)
+        self.assertEqual(self.response.status_code, 302)  # ログインしていないユーザーが数式検索ページにアクセスするとログインページにリダイレクトされる
         self.response = self.client.get(reverse('login:login'))
 
     def test_user_create_status_code(self):
@@ -32,27 +32,24 @@ class SuccessfulLoginTests(TestCase):
             'email': 'test@edu.cc.saga-u.ac.jp',
             'password': 'TestPass1',
         }
-        # The headers sent via **extra should follow CGI specification.
-        # CGI (Common Gateway Interface)に対応するためにヘッダー名の先頭に'HTTP_'を追加する
-        self.response = self.client.post(url, data, HTTP_ORIGIN='http://127.0.0.1:8000')
+        self.response = self.client.post(url, data)
         body_lines = mail.outbox[0].body.split('\n')
         url = body_lines[6]  # メール本文から認証urlを取得
         self.response = self.client.get(url)
 
         self.assertTrue(User.objects.get(email='test@edu.cc.saga-u.ac.jp'))
+        self.client.get(reverse('login:logout'))  # ユーザー登録時に自動的にログインされるのでログアウト
 
     def test_login(self):
-        self.response = self.client.get(reverse('login:login'))
+        login_url = reverse('login:login')
+        self.response = self.client.get(login_url)
         self.assertEqual(self.response.status_code, 200)
 
-        url = reverse('login:login')
         data = {
             'username': 'test@edu.cc.saga-u.ac.jp',
             'password': 'TestPass1',
         }
-        # The headers sent via **extra should follow CGI specification.
-        # CGI (Common Gateway Interface)に対応するためにヘッダー名の先頭に'HTTP_'を追加する\
-        self.response = self.client.post(url, data, HTTP_ORIGIN='http://127.0.0.1:8000')
+        self.response = self.client.post(login_url, data)
 
-        self.assertRedirects(self.response, reverse('search:index'))
+        self.assertRedirects(self.response, reverse('search:index'))  # ログインしたら数式検索ページにリダイレクト
         self.assertEqual(self.response.status_code, 302)
