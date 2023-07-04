24c9077a78ff1df8406066e234c3a10c96b2c198
Nakagawa Yuto <n.yuuto.11.11@docomo.ne.jp>
2023-07-04 19:20:35 +0900

    improve test_logout.py

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -15,18 +15,19 @@ class UserLogoutTests(TestCase):
     def setUp(self):
         """ユーザーを作成"""
         url = reverse('login:user_create')
+        self.test_email = 'test@edu.cc.saga-u.ac.jp'
         data = {
-            'email': 'test@edu.cc.saga-u.ac.jp',
+            'email': self.test_email,
             'password': 'TestPass1',
         }
         # The headers sent via **extra should follow CGI specification.
         # CGI (Common Gateway Interface)に対応するためにヘッダー名の先頭に'HTTP_'を追加する
-        self.response = self.client.post(url, data, HTTP_ORIGIN='http://127.0.0.1:8000')
+        self.client.post(url, data, HTTP_ORIGIN='http://127.0.0.1:8000')
         body_lines = mail.outbox[0].body.split('\n')
         auth_url = body_lines[8]  # メール本文から認証urlを取得
         self.response = self.client.get(auth_url)
 
-        self.assertTrue(User.objects.get(email='test@edu.cc.saga-u.ac.jp'))
+        self.assertTrue(User.objects.get(email=self.test_email))
         self.assertRedirects(self.response, reverse('search:index'))  # ユーザー登録時に自動的にログインされる
 
     def test_user_logout(self):
@@ -39,12 +40,13 @@ class EmailuserLogoutTests(TestCase):
     """Emailuserのログアウトをテスト"""
     @override_settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend')  # メールのテストのために上書き
     def setUp(self):
-        emailuser = EmailUser.objects.filter(email="test@edu.cc.saga-u.ac.jp")
+        self.test_email = 'test@edu.cc.saga-u.ac.jp'
+        emailuser = EmailUser.objects.filter(email=self.test_email)
         self.assertQuerysetEqual(emailuser, [])
 
         url = reverse('login:email_login')
         data = {
-            'email': 'test@edu.cc.saga-u.ac.jp',
+            'email': self.test_email,
         }
         # The headers sent via **extra should follow CGI specification.
         # CGI (Common Gateway Interface)に対応するためにヘッダー名の先頭に'HTTP_'を追加する
@@ -52,14 +54,14 @@ class EmailuserLogoutTests(TestCase):
 
         self.assertEqual(len(mail.outbox), 1)  # 1通のメールが送信されていること
         self.assertEqual(mail.outbox[0].from_email, '22801001@edu.cc.saga-u.ac.jp')  # 送信元
-        self.assertEqual(mail.outbox[0].to, ['test@edu.cc.saga-u.ac.jp'])  # 宛先
+        self.assertEqual(mail.outbox[0].to, [self.test_email])  # 宛先
 
         body_lines = mail.outbox[0].body.split('\n')
         auth_url = body_lines[7]  # メール本文から認証urlを取得
         self.response = self.client.get(auth_url)
 
         # ログインできているか確認
-        self.assertTrue(EmailUser.objects.get(email='test@edu.cc.saga-u.ac.jp').is_active)
+        self.assertTrue(EmailUser.objects.get(email=self.test_email).is_active)
         self.assertRedirects(self.response, reverse('search:index'))
 
     def test_emailuser_logout(self):
@@ -67,4 +69,4 @@ class EmailuserLogoutTests(TestCase):
         self.assertEqual(self.response.status_code, 302)
         self.assertRedirects(self.response, reverse('login:login'))
 
-        self.assertFalse(EmailUser.objects.get(email='test@edu.cc.saga-u.ac.jp').is_active)  # is_active=Falseを確認
+        self.assertFalse(EmailUser.objects.get(email=self.test_email).is_active)
